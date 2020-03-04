d8762c8ddf37d7fedb774d0256bdf664add734e6
Alessandro Viganò <alvistar@gmail.com>
2020-03-04 12:27:01 +0100

    Copy to clipboard

--- lib/ui/show_seed.dart
+++ lib/ui/show_seed.dart
@@ -4,6 +4,7 @@ import 'package:algorand_flutter/blocs/app_state.dart';
 import 'package:dart_algorand/dart_algorand.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ShowSeed extends StatelessWidget {
@@ -20,31 +21,47 @@ class ShowSeed extends StatelessWidget {
                 onPressed: () {
                   appBloc.add(AppBack());
                 })),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-                child: ListView(children: <Widget>[
-              ListTile(
-                title: Text(s.base.account.address),
-                subtitle: Text('Address'),
-                onTap: () => appBloc.add(AppSeedShow()),
-              ),
-              ListTile(
-                title: Text(s.base.account.private_key),
-                subtitle: Text('Private Key'),
-              ),
-                  ListTile(
-                    subtitle: Text('Seed'),
-                    title: Text(from_private_key(s.base.account.private_key))
-                  )
-            ])),
-            !s.forwardable
-                ? SizedBox.shrink()
-                : RaisedButton(
-                    child: Text('OKAY'),
-                    onPressed: () => appBloc.add(AppForward()),
-                  )
-          ],
+        body: Builder(
+          builder: (BuildContext context) => Column(
+            children: <Widget>[
+              Expanded(
+                  child: ListView(children: <Widget>[
+                ListTile(
+                    title: Text(s.base.account.address),
+                    subtitle: Text('Address'),
+                    trailing:
+                        copyWidget(s.base.account.address, context: context)),
+                ListTile(
+                  title: Text(s.base.account.private_key),
+                  subtitle: Text('Private Key'),
+                  trailing:
+                      copyWidget(s.base.account.private_key, context: context),
+                ),
+                ListTile(
+                  subtitle: Text('Seed'),
+                  title: Text(from_private_key(s.base.account.private_key)),
+                  trailing: copyWidget(
+                      from_private_key(s.base.account.private_key),
+                      context: context),
+                )
+              ])),
+              !s.forwardable
+                  ? SizedBox.shrink()
+                  : RaisedButton(
+                      child: Text('OKAY'),
+                      onPressed: () => appBloc.add(AppForward()),
+                    )
+            ],
+          ),
         ));
   }
 }
+
+copyWidget(String text, {BuildContext context}) => IconButton(
+      icon: Icon(Icons.content_copy),
+      onPressed: () {
+        Clipboard.setData(ClipboardData(text: text));
+        Scaffold.of(context)
+            .showSnackBar(SnackBar(content: Text('Copied to clipboard.')));
+      },
+    );
